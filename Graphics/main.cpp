7470e8dd9ac5943ed866aec02eb06966836bc1c9
E1P3 <js7656@gmail.com>
2022-10-08 23:12:24 +0100

    add shader implementation

--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -1,19 +1,21 @@
+#include <glm/glm.hpp>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <iostream>
+#include "shader.h"
+#include"VAO.h"
+#include"VBO.h"
+#include"EBO.h"
+
 
 // Macro for indexing vertex buffer
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
-GLuint shaderProgramID[2];
+Shader Shaders[2];
 GLuint VAO[2];
 GLuint VBO[2];
 GLuint EBO[2];
 
-// Vertex Shader (for convenience, it is defined in the main here, but we will be using text files for shaders in future)
-// Note: Input to this shader is the vertex positions that we specified for the triangle. 
-// Note: gl_Position is a special built-in variable that is supposed to contain the vertex position (in X, Y, Z, W)
-// Since our triangle vertices were specified as vec3, we just set W to 1.0.
 static const char* pVS = "                                                   \n\
 #version 330                                                                 \n\
                                                                              \n\
@@ -51,83 +53,35 @@ FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);									 \n\
 }"
 };
 
-
-// Shader Functions- click on + to expand
-#pragma region SHADER_FUNCTIONS
-static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+// Create 2 sets of 3 vertices to make up 2 triangles that fits on the viewport
+glm::vec3 vertices[][3] =
 {
-	// Create a shader object
-	GLuint ShaderObj = glCreateShader(ShaderType);
-
-	if (ShaderObj == 0)
 	{
-		fprintf(stderr, "Error creating shader type %d\n", ShaderType);
-		exit(0);
-	}
-	// Bind the source code to the shader, this happens before compilation
-	glShaderSource(ShaderObj, 1, (const GLchar**)&pShaderText, NULL);
-	// Compile the shader and check for errors
-	glCompileShader(ShaderObj);
-	GLint success;
-	// Check for shader related errors using glGetShaderiv
-	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
-	if (!success)
+		glm::vec3(-0.5f,  0.5f, 0.0f),
+		glm::vec3(0.5f, -0.5f, 0.0f),
+		glm::vec3(-0.5f, -0.5f, 0.0f)
+	},
 	{
-		GLchar InfoLog[1024];
-		glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
-		fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
-		exit(1);
+		glm::vec3(-0.5f,  0.5f, 0.0f),
+		glm::vec3(0.5f, -0.5f, 0.0f),
+		glm::vec3(0.5f,  0.5f, 0.0f)
 	}
-	// Attach the compiled shader object to the program object
-	glAttachShader(ShaderProgram, ShaderObj);
-}
+};
 
-GLuint CompileShaders(static const char* pVS, static const char* pFS)
+// Create a color array that identfies the colors of each vertex (format R, G, B, A)
+// Note: The color array will be ignored by the fragment shader for the second triangle
+glm::vec4 colors[] =
 {
-	// Start the process of setting up our shaders by creating a program ID
-	// Note: we will link all the shaders together into this ID
-	GLuint shaderProgramID = glCreateProgram();
-	if (shaderProgramID == 0)
-	{
-		fprintf(stderr, "Error creating shader program\n");
-		exit(1);
-	}
+		glm::vec4(1.0f, 0.0f, 0.0f, 1.0f),
+		glm::vec4(0.0f, 1.0f, 0.0f, 1.0f),
+		glm::vec4(0.0f, 0.0f, 1.0f, 1.0f)
+};
 
-	// Create two shader objects, one for the vertex, and one for the fragment shader
-	AddShader(shaderProgramID, pVS, GL_VERTEX_SHADER);
-	AddShader(shaderProgramID, pFS, GL_FRAGMENT_SHADER);
-
-	GLint Success = 0;
-	GLchar ErrorLog[1024] = { 0 };
-
-
-	// After compiling all shader objects and attaching them to the program, we can finally link it
-	glLinkProgram(shaderProgramID);
-	// Check for program related errors using glGetProgramiv
-	glGetProgramiv(shaderProgramID, GL_LINK_STATUS, &Success);
-	if (Success == 0)
-	{
-		glGetProgramInfoLog(shaderProgramID, sizeof(ErrorLog), NULL, ErrorLog);
-		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
-		exit(1);
-	}
-
-	// Program has been successfully linked but needs to be validated to check whether the program can execute given the current pipeline state
-	glValidateProgram(shaderProgramID);
-	// Check for program related errors using glGetProgramiv
-	glGetProgramiv(shaderProgramID, GL_VALIDATE_STATUS, &Success);
-	if (!Success)
-	{
-		glGetProgramInfoLog(shaderProgramID, sizeof(ErrorLog), NULL, ErrorLog);
-		fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
-		exit(1);
-	}
-	// Finally, use the linked shader program
-	// Note: this program will stay in effect for all draw calls until you replace it with another or explicitly disable its use
-	glUseProgram(shaderProgramID);
-	return shaderProgramID;
-}
-#pragma endregion SHADER_FUNCTIONS
+// Create an index buffer for the 2 triangles
+GLuint indices[] =
+{
+		0, 1, 2
+};
 
 // VBO Functions - click on + to expand
 #pragma region VBO_FUNCTIONS
@@ -170,11 +124,11 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT);
 	// NB: Make the call to draw the geometry in the currently activated vertex buffer. This is where the GPU starts to work!
 	// Need to call shader here
-	glUseProgram(shaderProgramID[0]);
+	Shaders[0].Activate();
 	glBindVertexArray(VAO[0]);
 	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
-	glUseProgram(shaderProgramID[1]);
+	Shaders[1].Activate();
 	glBindVertexArray(VAO[1]);
 	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
@@ -184,35 +138,6 @@ void display()
 
 void init()
 {
-	// Create 2 sets of 3 vertices to make up 2 triangles that fits on the viewport
-	GLfloat vertices[][9] =
-	{
-		{
-			-0.5f,  0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			-0.5f, -0.5f, 0.0f
-		},
-		{
-			-0.5f,  0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.5f,  0.5f, 0.0f
-		}
-	};
-
-	// Create a color array that identfies the colors of each vertex (format R, G, B, A)
-	// Note: The color array will be ignored by the fragment shader for the second triangle
-	GLfloat colors[] =
-	{
-			1.0f, 0.0f, 0.0f, 1.0f,
-			0.0f, 1.0f, 0.0f, 1.0f,
-			0.0f, 0.0f, 1.0f, 1.0f
-	};
-
-	// Create an index buffer for the 2 triangles
-	GLuint indices[] =
-	{
-			0, 1, 2
-	};
 
 	// Allocate memory to vertex object arrays and index buffers
 	glGenVertexArrays(2, VAO);
@@ -220,11 +145,11 @@ void init()
 	for (int i = 0; i < 2; i++)
 	{
 		// Set up the shaders
-		shaderProgramID[i] = CompileShaders(pVS, pFS[i]);
+		Shaders[i].CompileVF(pVS, pFS[i]);
 		// Put the vertices and colors into a vertex buffer object
-		generateObjectBuffer(VBO[i], vertices[i], colors);
+		generateObjectBuffer(VBO[i], (float*)vertices[i], (float*)colors);
 		// Link the current buffer to the shader
-		linkCurrentBuffertoShader(VAO[i], VBO[i], shaderProgramID[i]);
+		linkCurrentBuffertoShader(VAO[i], VBO[i], Shaders[i].ID);
 		// Link the current index buffer to the vertex array object
 		glBindVertexArray(VAO[i]);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[i]);
